a7839f83eb6ab4d952821822be145728929eec17
myothiha09 <myothiha09@gatech.edu>
2017-02-12 15:44:12 -0500

    View files edited

--- app/src/main/java/com/example/myothiha09/m4cs2340/WelcomeActivity.java
+++ app/src/main/java/com/example/myothiha09/m4cs2340/WelcomeActivity.java
@@ -1,13 +1,21 @@
 package com.example.myothiha09.m4cs2340;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class WelcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
+        setContentView(R.layout.welcome_screen);
+    }
+    public void onClick(View v) {
+        if (v.getId() == R.id.loginButton) {
+            Intent intent = new Intent(this, LoginAcitivity.class);
+            startActivity(intent);
+        }
     }
 }
