39902b194bc00987ca1cfb0946ae6d3135d8e3de
myothiha09 <myothiha09@gatech.edu>
2017-02-12 15:59:36 -0500

    View files edited

--- app/src/main/java/com/example/myothiha09/m4cs2340/controller/LoginAcitivity.java
+++ app/src/main/java/com/example/myothiha09/m4cs2340/controller/LoginAcitivity.java
@@ -2,12 +2,17 @@ package com.example.myothiha09.m4cs2340.controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class LoginAcitivity extends AppCompatActivity {
-
+    EditText username;
+    EditText password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_screen);
+    }
+    private boolean checkPassword() {
+
     }
 }
