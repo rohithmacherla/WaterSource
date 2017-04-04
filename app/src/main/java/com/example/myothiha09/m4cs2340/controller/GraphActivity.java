30360b305e2e713eea0e3d8214c68be482925c4b
Myo Thiha <Myo Thiha>
2017-04-03 20:43:53 -0400

    Graph Stuffs

---
+++ app/src/main/java/com/example/myothiha09/m4cs2340/controller/GraphActivity.java
@@ -0,0 +1,15 @@
+package com.example.myothiha09.m4cs2340.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.myothiha09.m4cs2340.R;
+
+public class GraphActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graph);
+    }
+}
---
+++ app/src/main/java/com/example/myothiha09/m4cs2340/controller/GraphFragment.java
@@ -0,0 +1,8 @@
+package com.example.myothiha09.m4cs2340.controller;
+
+/**
+ * Created by myothiha09 on 4/3/2017.
+ */
+
+public class GraphFragment {
+}
